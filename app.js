9167c5f7c2a8fee0d41f603386cca46781a7890e
unknown <akung@CUTSP13.ESRI.local>
2016-01-05 06:59:33 -0800

    rankings

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ require([
 "esri/dijit/editing/TemplatePicker", "esri/dijit/TimeSlider",
 "esri/renderers/TimeClassBreaksAger", "esri/renderers/TemporalRenderer",
 "dojo/parser", "dojo/_base/array", "esri/Color", "dojo/dom", "dojo/date",
-"esri/tasks/query","esri/tasks/QueryTask",
+"esri/tasks/query","esri/tasks/QueryTask","esri/tasks/StatisticDefinition",
 
 "dijit/layout/BorderContainer", "dijit/layout/ContentPane", "dojo/domReady!"
 ], function(
@@ -17,7 +17,7 @@ SimpleMarkerSymbol, SimpleLineSymbol,
 TemplatePicker, TimeSlider,
 TimeClassBreaksAger, TemporalRenderer,
 parser, arrayUtils, Color, dom, date,
-Query, QueryTask
+Query, QueryTask, StatisticDefinition
 ) {
 parser.parse();
 
@@ -47,31 +47,111 @@ function mapLoaded() {
   
 
   featureLayer.setTimeDefinition(timeExtent);
-  featureLayer.on("load", featureLayerLoaded);
 
- 
+
   map.addLayer(featureLayer);
   
- 
-  
-  
-/*   //QUERY
-var qry = new Query();
-qry.outFields = ["TeamName","Points"];
-
-var statDef = new StatisticDefinition();
-statDef.statisticType = "sum";
-statDef.onStatisticField = "Points"
-qry.outStatistics = [statDef];
-qry.groupByFieldsForStatistics["TeamName"];*/
+	loadRanks();
+	
+	  
+  //$("#timeBtn").on("click",featureLayerLoaded(featureLayer));   
   
 }
 
+function loadRanks(){
+	
+		//QUERY
+	
+	var qryTask = new QueryTask("http://services.arcgis.com/EgePHk52tsFjmhbJ/arcgis/rest/services/TESTTeamGuess/FeatureServer/0");
+	
+	var qry = new Query();
+	qry.where = "OBJECTID > 0";
+	qry.outFields = ["*"];
+
+	
+	qryTask.execute(qry, function(fs){
+		
+		//get list of Teams
+		var teamList = [];
+		
+		
+		for(var i = 0; i<fs.features.length; i++){
+			
+			var team = fs.features[i].attributes.TeamName;	
+
+			teamList.push(team);
+
+		}
+		
+		function onlyUnique(value, index, self) { 
+			return self.indexOf(value) === index;
+		}
+
+		var teamFinal = teamList.filter( onlyUnique );
+		
+		var listOfObjects = [];
+		
+		
+		teamFinal.forEach(function(entry) {
+			
+				var singleObj = {};
+				singleObj['team'] = entry;
+				singleObj['pts'] = 0;
+				
+				listOfObjects.push(singleObj);
+				
+		});
+		
+		
+
+		for(var i = 0; i<fs.features.length; i++){
+			
+			var team = fs.features[i].attributes.TeamName;
+			var point = fs.features[i].attributes.Points;
+
+ 			//for each team in the list of teams
+			
+			for(var k = 0; k<listOfObjects.length; k++){
+				
+				if (listOfObjects[k]["team"] == team){
+								
+					listOfObjects[k]["pts"] += point;
+	
+				}
+				
+			} 
+			
+		}
+		
+		var divtable = document.getElementById('rankTble');
+		var rankTable='';
+		
+		listOfObjects.forEach(function(tm){
+			
+			var teamName = tm['team'];
+			var totalPts = tm['pts'];
+	
+			
+			//append to div
+			rankTable += '<tr><td>'+teamName+'</td><td>'+totalPts+'</td></tr>';
+			
+			divtable.innerHTML = rankTable;
+			
+		});
+		
+		
+		
+	});
+	
+}
+
 function featureLayerLoaded(evt) {
+	
+	console.log("loadddd");
   // creates time slider
   timeSlider = new TimeSlider({ style: "width: 100%;"}, dom.byId("timeSliderDiv"));
   timeSlider.setThumbCount(1);
-  timeSlider.createTimeStopsByTimeInterval(evt.layer.getTimeDefinition(), 5, TimeInfo.UNIT_SECONDS);
+  timeSlider.createTimeStopsByTimeInterval(evt.layer.getTimeDefinition(), 60, TimeInfo.UNIT_SECONDS);
   timeSlider.setThumbIndexes([0]);
   timeSlider.on("time-extent-change", displayTimeInfo);
   timeSlider.startup();
